3eab1c7590404dd1dafc4b4e2490811150261663
Kesi Maduka <me@kez.io>
2016-02-10 14:48:53 -0600

    update stats

--- public_html/ajax/statistics.php
+++ public_html/ajax/statistics.php
@@ -1,6 +1,10 @@
 <?php
 require_once('/home/mhsa/includes/include.php');
 
- echo json_encode(array('registered_users' => getTotalNumberOfPlayers(), 'alive_players' => getNumberOfPlayersAlive(), 'dead_players' => getNumberOfPlayersDead(), 'suicides' => getNumberOfPlayersSuicide(), 'assassinations' => getNumberOfKills()));
-//{"registered_users":"472","alive_players":"1","dead_players":471,"suicides":"56","assassinations":"427","top10":[["Nyssa Turner","27","1"],["Grant Jones","26","1"],["Josh Watson","20","1"],["Mackensie Bragg","10","1"],["Kyiva Faith","9","1"],["Chase Montgomery","9","1"],["Crystal Kim","9","1"],["Travis Murray","8","1"],["Darrien Murphy","8","1"],["Matt Brinegar","8","1"]]}
+$top10 = array();
+foreach($user in getTop10Players()) {
+  $top10[] = array($user['name'].(strlen($user['twitter_name']) > 0 ? ' '.$user['twitter_name'] : ''), $user['num_kills'], $user['dead']);
+}
+
+ echo json_encode(array('registered_users' => getTotalNumberOfPlayers(), 'alive_players' => getNumberOfPlayersAlive(), 'dead_players' => getNumberOfPlayersDead(), 'suicides' => getNumberOfPlayersSuicide(), 'assassinations' => getNumberOfKills(), 'top10' => $top10));
 ?>
