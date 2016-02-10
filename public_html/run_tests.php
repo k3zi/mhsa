0801450b7fa9b30d786028feee5048ee2916ab3e
Kesi Maduka <me@kez.io>
2016-02-10 11:31:00 -0600

    Update run_tests.php

--- public_html/run_tests.php
+++ public_html/run_tests.php
@@ -37,8 +37,8 @@ echo $spacer;
 echo '2. Test Get Available Assassin:'.'<br>';
 echo("<pre>".print_r(getAvailableAssassin(), true)."</pre>");
 
-echo '3. Test Call:'.'<br>';
-echo("<pre>".print_r(singleCall('8177297784', "https://mhsa.io/api/johncena_response.xml"), true)."</pre>");
+//echo '3. Test Call:'.'<br>';
+//echo("<pre>".print_r(singleCall('8177297784', "https://mhsa.io/api/johncena_response.xml"), true)."</pre>");
 
 echo $spacer;
 
