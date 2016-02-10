00c3233c2c477c4ebf820833e97b49ce0f19932c
Kesi Maduka <me@kez.io>
2016-02-10 14:53:19 -0600

    Update statistics.php

--- public_html/ajax/statistics.php
+++ public_html/ajax/statistics.php
@@ -3,7 +3,7 @@ require_once('/home/mhsa/includes/include.php');
 
 $top10 = array();
 foreach(getTop10Players() as $user) {
-  $top10[] = array($user['name'].(strlen($user['twitter_name']) > 0 ? ' '.$user['twitter_name'] : ''), $user['num_kills'], $user['dead']);
+  $top10[] = array($user['name'].(strlen($user['twitter_name']) > 0 ? ' (<a target="_blank" href="https://twitter.com/'.$user['twitter_name'].'"> @'.$user['twitter_name'].'</a>)' : ''), $user['num_kills'], $user['dead']);
 }
 
  echo json_encode(array('registered_users' => getTotalNumberOfPlayers(), 'alive_players' => getNumberOfPlayersAlive(), 'dead_players' => getNumberOfPlayersDead(), 'suicides' => getNumberOfPlayersSuicide(), 'assassinations' => getNumberOfKills(), 'top10' => $top10));
