848cfd9a56154543744c86b58e0b9855a43ae13b
Kesi Maduka <me@kez.io>
2016-04-07 14:59:57 -0500

    add hash constant

--- public_html/api/github.php
+++ public_html/api/github.php
@@ -1,7 +1,5 @@
 <?php
-// Copyright 2014 Peter Beverloo. All rights reserved.
-// Use of this source code is governed by the MIT license, a copy of which can
-// be found in the LICENSE file.
+require_once('/home/mhsa/includes/config.php');
 
 if (!isset ($_SERVER['HTTP_X_GITHUB_EVENT']))
     die('-1');
@@ -14,8 +12,13 @@ if (strpos($signature, '=') == false)
     die('-4');
 list($algorithm, $hash) = explode('=', $signature, 2);
 $payload = file_get_contents('php://input');
-if (hash_hmac($algorithm, $payload, '8b21ebc436b7c5c7fe54953c0553ab9a') != $hash)
-    die('Algo: '.$algorithm."\n\n".hash_hmac($algorithm, $payload, '8b21ebc436b7c5c7fe54953c0553ab9a').'   !=   '.$hash);
+
+$hashAnswer = hash_hmac($algorithm, $payload, MHSA_GITHUB_HOOK_PASS);
+
+if ($hashAnswer != $hash) {
+    die('Algo: '.$algorithm."\n\n".$hashAnswer.'   !=   '.$hash);
+}
+
 // -------------------------------------------------------------------------------------------------
 
 echo shell_exec("/usr/bin/git --git-dir '/home/mhsa/.git/' --work-tree '/home/mhsa/' fetch --all 2>&1");
